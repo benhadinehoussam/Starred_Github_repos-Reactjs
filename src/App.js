cc7a3857dc0db001ffa76519c662f9937b882d56
HoussamBenhadine <houssambenhadine21@gmail.com>
2020-01-03 20:36:18 +0100

    get the data back

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { getReposIds } from './services/GitHubAPI'
 
-export const  App = () => <p>Hello</p>
+export const  App = () => {
+    
+    // hold data after calling an API and prevent blocking the UI rendering; app more interactive
+    
+    //initiliaze where to store data
+    const [reposIds,setReposIds] = useState([]);
+
+    // update the value of repos ID, call the setter ;-)
+    useEffect(() => {
+        getReposIds().then(repo => setReposIds(repo));
+    }, []);
+    return(
+        <p>{JSON.stringify(reposIds)}</p>
+    )
+
+
+};
  
