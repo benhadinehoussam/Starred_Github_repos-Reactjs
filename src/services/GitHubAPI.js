c2f6f8be7a86e92fb249f3b9b76b5002d6210bb4
HoussamBenhadine <houssambenhadine21@gmail.com>
2020-01-04 17:32:03 +0100

    the JSON response that will be rendered

--- src/services/GitHubAPI.js
+++ src/services/GitHubAPI.js
@@ -5,24 +5,34 @@ export const baseUrl = `https://api.github.com/search/`;
 export const reposUrl = `${baseUrl}repositories?q=created:>2017-10-22&sort=stars&order=desc`;
 
 export const getReposIds = async () => {
+    
     let resp = {};
-
     // results is a promise need to be resolved
     const results = await axios.get(reposUrl).then(({ data }) => data.items);
     results.forEach(element => {
     // console.log(element);
-    resp[element.name] = {};
-    resp[element.name]["Description"] = element.description;
-    resp[element.name]["Avatar"] = element.owner.avatar_url;
-    resp[element.name]["Stars"] = element.stargazers_count;
-    resp[element.name]["Issues"] = element.open_issues_count;
-    resp[element.name]["Time Interval"] = element.updated_at;
+        
+    resp[element.id] =   {
+        name: element.name,
+        description: element.description,
+        avatar: element.owner.avatar_url,
+        stars: element.stargazers_count,
+        issues: element.open_issues_count,
+        time_interval: element.updated_at
+    };
 
+    // {description: element.description};
+    
+    // resp["Description"] = element.description;
+    // resp["Avatar"] = element.owner.avatar_url;
+    // resp["Stars"] = element.stargazers_count;
+    // resp["Issues"] = element.open_issues_count;
+    // resp["Time Interval"] = element.updated_at;
 
-
+    
 
 
     });
-
+    // Object.entries(resp).map(repo => console.log(repo[1]));
     return resp;
 }
